b521e6f2c20ea4c7469a89a287af3defb5433ca3
Elena Tolkacheva <hvs-fasya@mail.ru>
2018-05-27 17:39:22 +0300

    the first intermediate template

--- news/home.go
+++ news/home.go
@@ -2,9 +2,12 @@ package news
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/miniflux/miniflux/http/context"
+	"github.com/miniflux/miniflux/http/request"
 	"github.com/miniflux/miniflux/http/response/html"
+	"github.com/miniflux/miniflux/model"
 	"github.com/miniflux/miniflux/ui/session"
 	"github.com/miniflux/miniflux/ui/view"
 	"github.com/miniflux/miniflux/version"
@@ -19,6 +22,33 @@ func (c *Controller) Home(w http.ResponseWriter, r *http.Request) {
 	view.Set("version", version.Version)
 	view.Set("build_date", version.BuildDate)
 	view.Set("menu", "settings")
+
+	builder := c.store.NewNewsEntryQueryBuilder()
+	offset := request.QueryIntParam(r, "offset", 0)
+	builder.WithoutStatus(model.EntryStatusRemoved)
+	builder.WithOrder(model.DefaultSortingOrder)
+	builder.WithDirection(model.DefaultSortingDirection)
+	builder.WithOffset(offset)
+	builder.WithLimit(nbItemsPerPage)
+
+	monthBefore := time.Now().AddDate(0, -1, 0)
+	builder.After(&monthBefore)
+
+	entries, err := builder.GetEntries()
+	if err != nil {
+		html.ServerError(w, err)
+		return
+	}
+
+	count, err := builder.CountEntries()
+	if err != nil {
+		html.ServerError(w, err)
+		return
+	}
+
+	//view.Set("current", current)
+	view.Set("entries", entries)
+	view.Set("total", count)
 
 	html.OK(w, view.NewsRender("news_home"))
 }
